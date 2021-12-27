01208b9c29e50dd64f5c2b1c4b6139fd1ada6e58
Anna <58006989+Anna-Z-Krk@users.noreply.github.com>
2021-12-27 09:39:57 +0100

    Add files via upload

---
+++ js/aossettings.js
@@ -0,0 +1,7 @@
+AOS.init(
+    {
+        startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+        offset: 200, // offset (in px) from the original trigger point
+        once: true, // whether animation should happen only once - while scrolling down
+    }
+);
